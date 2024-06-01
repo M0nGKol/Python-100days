fe12bf61c6478d0cfd2c3d7bc44a6b179b7e6179
Apple <apple@Apples-MacBook-Pro.local>
2024-06-01 23:45:05 +0700

    if elif else

---
+++ day_3.py
@@ -0,0 +1,226 @@
+#if Elif Else statments
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+bill = 0
+
+if height >= 120:
+  print("You can ride the rollercoaster!")
+  age = int(input("What is your age? "))
+  if age < 12:
+    bill = 5
+    print("Child tickets are $5.")
+  elif age <= 18:
+    bill = 7
+    print("Youth tickets are $7.")
+  else:
+    bill = 12
+    print("Adult tickets are $12.")
+
+  wants_photo = input("Do you want a photo taken? Y or N. ")
+  if wants_photo == "Y":
+    bill += 3
+
+  print(f"Your final bill is ${bill}")
+
+else:
+  print("Sorry, you have to grow taller before you can ride.")
+
+
+#Exercise 1
+'''
+Write a program that works out whether if a given number is an odd or even number.
+
+Even numbers can be divided by 2 with no remainder.
+
+e.g. 86 is even because 86 ÷ 2 = 43
+
+43 does not have any decimal places. Therefore the division is clean.
+
+e.g. 59 is odd because 59 ÷ 2 = 29.5
+
+29.5 is not a whole number, it has decimal places. Therefore there is a remainder of 0.5, so the division is not clean.
+'''
+number = int(input("Enter number: "))
+if number%2 == 0:
+  print("This is an even number.")
+else:
+  print("This is an odd number.")
+
+'''
+Exercise 2
+Write a program that interprets the Body Mass Index (BMI) based on a user's weight and height.
+
+It should tell them the interpretation of their BMI based on the BMI value.
+
+Under 18.5 they are underweight
+Equal to or over 18.5 but below 25 they have a normal weight
+Equal to or over 25 but below 30 they are slightly overweight
+Equal to or over 30 but below 35 they are obese
+Equal to or over 35 they are clinically obese.
+'''
+height = float(input())
+
+weight = int(input())
+BMI = weight / (height ** 2)
+if BMI < 18.5:
+    print(f"Your BMI is {BMI}, you are underweight")
+elif BMI < 25:
+    print(f"Your BMI is {BMI}, you have a normal weight.")
+elif BMI < 30:
+    print(f"Your BMI is {BMI}, you are slightly overweight.")
+elif BMI < 35:
+    print(f"Your BMI is {BMI}, you are obese.")
+else:
+    print(f"Your BMI is {BMI}, you are clinically obese.")
+
+'''
+Exercise 3
+Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February.
+
+This is how you work out whether if a particular year is a leap year.
+
+on every year that is divisible by 4 with no remainder
+
+except every year that is evenly divisible by 100 with no remainder
+
+unless the year is also divisible by 400 with no remainder
+
+'''
+if year%4 == 0:
+  if year%100 ==0:
+    if year%400 ==0:
+      print("Leap year")
+    else:
+      print("Not leap year")
+  else:
+    print("Leap year")
+else:
+  print("Not leap year")
+
+'''
+Based on a user's order, work out their final bill.
+
+Small pizza (S): $15
+
+Medium pizza (M): $20
+
+Large pizza (L): $25
+
+Add pepperoni for small pizza (Y or N): +$2
+
+Add pepperoni for medium or large pizza (Y or N): +$3
+
+Add extra cheese for any size pizza (Y or N): +$1
+'''
+print("Thank you for choosing Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L")
+add_pepperoni = input("Do you want pepperoni? Y or N")
+extra_cheese = input("Do you want pepperoni? Y or N")
+
+if size == "S":
+  bill = 15
+elif size == "M":
+  bill = 20
+else :
+  bill =25
+if size == "S" and add_pepperoni == "Y":
+  bill += 2
+elif size == "M" or "L" and add_pepperoni =="Y":
+  bill += 3
+
+if extra_cheese == "Y":
+  bill +=1
+
+print(f"Your final bill is: ${bill}.")
+
+'''
+You are going to write a program that tests the compatibility between two people.
+
+To work out the love score between two people:
+
+Take both people's names and check for the number of times the letters in the word TRUE occurs.
+
+Then check for the number of times the letters in the word LOVE occurs.
+
+Then combine these numbers to make a 2 digit number.
+
+For Love Scores less than 10 or greater than 90, the message should be:
+
+"Your score is *x*, you go together like coke and mentos."
+For Love Scores between 40 and 50, the message should be:
+
+"Your score is *y*, you are alright together."
+Otherwise, the message will just be their score. e.g.:
+
+"Your score is *z*."
+
+'''
+
+print("The Love Calculator is calculating your score...")
+name1 = input("What is your name?")
+name2 = input("What is their name?")
+name = name1 + name2
+name_low = name.lower()
+T = name_low.count("t")
+R = name_low.count("r")
+U = name_low.count("u")
+E = name_low.count("e")
+total_true = T + R + U + E
+L = name_low.count("l")
+O = name_low.count("o")
+V = name_low.count("v")
+E = name_low.count("e")
+total_loveee = L + O + V + E
+Love_score = int(str(total_true) + str(total_loveee))
+if Love_score <10 or Love_score >90:
+  print(f"Your score is {Love_score}, you go together like coke and mentos.")
+elif Love_score >40 and Love_score <50:
+  print(f"Your score is {Love_score}, you are alright together.")
+else:
+  print(f"Your score is {Love_score}.")
+
+
+#Treasure game :
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+choice1 = input('You\'re at a cross road. Where do you want to go? Type "left" or "right" \n').lower()
+if choice1 == "left":
+  choice2 = input('You\'ve come to a lake. There is an island in the middle of the lake. Type "wait" to wait for a boat. Type "swim" to swim across. \n').lower()
+  if choice2 == "wait":
+    choice3 = input("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose? \n").lower()
+    if choice3 == "red":
+      print("It's a room full of fire. Game Over.")
+    elif choice3 == "yellow":
+      print("You found the treasure! You Win!")
+    elif choice3 == "blue":
+      print("You enter a room of beasts. Game Over.")
+    else:
+      print("You chose a door that doesn't exist. Game Over.")
+  else:
+    print("You get attacked by an angry trout. Game Over.")
+else:
+  print("You fell into a hole. Game Over.")
